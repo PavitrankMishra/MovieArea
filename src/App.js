aa2be168b1e5ffa7ec227fd4b6d0ad01c0faec41
Pavitrank <pavitrank2002@gmail.com>
2024-04-28 14:55:30 +0530

    Changed the fetching request to safe connection

--- src/App.js
+++ src/App.js
@@ -141,7 +141,7 @@ export default function App() {
           setisLoading(true);
           setError("");
           const res = await fetch(
-            `http://omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok) {
@@ -354,7 +354,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
